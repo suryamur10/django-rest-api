b05722c1bff99cac6bad620f508ca0e8247635ec
Suryamur10 <suryamur@buffalo.edu>
2025-06-26 14:29:26 -0700

    Initial Django REST API commit

---
+++ api/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Message(models.Model):
+    text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
---
+++ api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from .serializers import MessageSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Message
+from .serializers import MessageSerializer
+import logging
+
+logger = logging.getLogger(__name__)
+
+@api_view(['GET', 'POST'])
+def message_list(request):
+    if request.method == 'GET':
+        logger.info("GET /messages called")
+        messages = Message.objects.all()
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        logger.info("POST /messages called with data: %s", request.data)
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        logger.error("POST /messages error: %s", serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Create your views here.
